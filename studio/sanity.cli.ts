d100bd94b8535565d3b9c5f8253a587b748f9865
Tyler Fonda <tyler@blackstrap.it>
2025-10-02 15:42:17 -0400

    Update sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -4,10 +4,9 @@
  * and customizes the Vite bundler configuration.
  * Learn more: https://www.sanity.io/docs/cli
  */
-
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'nrhwzduc'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineCliConfig({
@@ -15,6 +14,9 @@ export default defineCliConfig({
     projectId,
     dataset,
   },
-  studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to learn more about using environment variables for local & production.
+  studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '',
+  deployment: {
+    appId: 'pnmp2ahumcxc7etcxyghzqpe',
+  },
   autoUpdates: true,
 })
